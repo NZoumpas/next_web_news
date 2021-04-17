6e9132c0842cc9a50509c7c0a6bb1351a9525677
Nikolaos <nzoump@gmail.com>
2021-04-17 08:06:45 +0200

    Init commit

---
+++ components/Meta.js
@@ -0,0 +1,21 @@
+import Head from "next/head";
+
+const Meta = ({ title, keywords, description }) => {
+  return (
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="keywords" content={keywords} />
+      <meta name="description" content={description} />
+      <meta charset="UTF-8" />
+      <link rel="icon" href="/favicon.ico" />
+      <title>{title}</title>
+    </Head>
+  );
+};
+
+Meta.defaultProps = {
+  title: "WbDev Newz",
+  keywords: "web developer, programming",
+  description: "Get the latest news in web edv",
+};
+export default Meta;
